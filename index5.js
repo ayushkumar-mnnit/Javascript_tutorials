9e71ae0bae51ba44af48e6312a5bfab3263ab1e5
ayushkumar-mnnit <ayushk.2125@gmail.com>
2023-06-05 10:13:46 +0530

    modified index5.js

--- index5.js
+++ index5.js
@@ -16,30 +16,122 @@ so in modern JS we mainly focus on ES-6
 
 */
 
+//Modern JS includes: let-const keywords,template literals,default arguments,rest operators,destructuring,object properties,arrow function,spread operators.
 
-// let vs const vs var keywords:
+
+// 1. let vs const vs var keywords in :
 
 /* let and const are part of modern JS,
 
 let : block scope--> can't change outside a particular block
 
-var : function scope--> can't change outside a function
+var : global scope or function scope--> if defined globally then can be used and modified anywhere in the program but if defined within a function then can be used and modified within that function only.
 
 const : block scope--> can't change once declared
 
 */
 
-// function sum(a,b){
-//     var x=3
-//   console.log(a+b)
-// }
-// // x=5
-// console.log(x)
+// defining variable globally:
+var x=5 
+console.log(x)  // accessible outside anywhere in the program
+x=3
+console.log(x)  // accessible and modified, outside anywhere in the program
 
-if(true)
+function sum()
 {
-    let a=23
+    console.log(x) // accessible within function also
+    x=7     // modified within function also
+    console.log(x)
 }
-console.log(a)
-let a=34
-console.log(a)
+sum()
+
+//defining variable within a function specifically will restrict its access and modification within the function only 
+
+
+function sum1()
+{   var y=4
+    console.log(y) // accessible within function
+    y=8     // modified within function 
+    console.log(y)
+}
+sum1()
+
+// console.log(y)   this gives error becoz y is having a function scope only
+
+// let- has a block scope so it can not be accessed and modified outside a specific block.That's why it is an improved version of var keyword for declaration becoz this prevents accidental modifictions in the program
+
+
+
+
+{
+    let c=23
+    c=5
+    console.log(c) // access and modification possible within the block
+}
+
+// console.log(c)  access and modification not possible outside the block once defined within the block
+
+let d=9
+//  let d=8     redeclaration is not allowed in let but allowed in var
+console.log(d)
+
+
+// const- all properties same as let but additionally user can't change its value
+
+// const m  --> using without initialisation is not allowed but allowed in var and let
+
+
+
+/*
+
+In a nutshell main differneces among var,let,const are:
+
+var- global/function scope, can be accessed and modified within the scope (globally declared-use and modify globally but declared within a function then use and modify within that function only). redeclaration allowed,use without initialisation allowed
+
+let-block scope,can be accessed and modified within the scope i.e. within the block only. redeclaration not allowed,use without initialisation allowed.
+
+const-block scope,can be accessed but can't be modified within the scope i.e. within the block only. redeclaration not allowed,use without initialisation not allowed
+
+*/
+
+
+
+// 2.Template literlas (strings):
+
+
+console.log("sum of 3 and 4 is = "+(3+4))
+console.log(`sum of 3 and 4 is = ${3+4}`) // concatenation of string becomes easy using template literals
+
+// 3. Default arguments: these are the default values passed to the parameters when sufficient arguments are not passed to the function during function call.
+
+
+function sum2(a,b=6) 
+{
+    console.log(a+b)
+    
+}
+sum2(4) // here 2 arguements need to be passed but only 1 given so this will go to parameter a and b act as default argument
+
+// 4. Fat arrow function: => symbol is known as fat arrow 
+
+//example:
+
+let sum3=(a,b)=>
+{   
+
+    return `The sum is=${a+b}`
+
+}
+
+console.log(sum3(2,64))
+
+// if single value is to be returned then ,return keyword can be removed and curly braces as well so above function can be reduced as 
+
+let sum4=(p,q)=> `The sum is=${p+q}`
+console.log(sum4(5,6))
+
+// if single parameter is to be passed then parenthesis can be removed as
+
+let square=n=>`Square of entered number is=${n**2}`
+
+console.log(square(5))
